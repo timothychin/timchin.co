a3e9042d41a57996114a4d6a76f0ce23319782d0
Tim Chin <timchin@Tims-MacBook-Pro.local>
2017-02-24 18:08:56 -0800

    Adds skeleton for Portfolio component

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,25 @@
+var Portfolio = () => (
+  <div> 
+    <div className="photoBanner">
+      <img src=""/>
+    </div>
+    <div>
+      <div className="project-thumbnails">
+        <a>Nimbus</a>
+        <a href=""><img src=""/></a>      
+      </div>
+
+      <div className="project-thumbnails">
+        <a>TubeNotes</a>
+        <a><img src=""/></a> 
+      </div>
+
+      <div className="project-thumbnails">
+        <a>PlanThis</a>
+        <a><img src=""/></a> 
+      </div>
+    </div>
+  </div>
+);
+
+window.Portfolio = Portfolio;
